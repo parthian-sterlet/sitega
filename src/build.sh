96b882093467134697346e35cf71adcfaebd303d
Victor Levitsky <68056117+parthian-sterlet@users.noreply.github.com>
2021-12-24 10:56:01 +0700

    Update build.sh

--- src/build.sh
+++ src/build.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 g++ -o sitega_thr_dist_mat sitega_thr_dist_mat.cpp
-g++ -o andy0bsn2 andy0bsn2.cpp
+g++ -o andy0bsn5 andy0bsn5.cpp
 g++ -o andy1_mat andy1_mat.cpp
-g++ -o andy02 andy02.cpp
+g++ -o andy05 andy05.cpp
 g++ -o background_genome background_genome.cpp
-chmod a+x andy0bsn2 
+chmod a+x andy0bsn5 
 chmod a+x andy1_mat
-chmod a+x andy02
+chmod a+x andy05
 chmod a+x monte0dg
 chmod a+x sitega_thr_dist_mat
 chmod a+x bootstrap
