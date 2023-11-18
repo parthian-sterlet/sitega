240f26c8e7ba48ad73f66725b88cd674a1ebe53d
Victor Levitsky <68056117+parthian-sterlet@users.noreply.github.com>
2023-11-18 16:32:26 +0700

    Update build.sh

--- src/build.sh
+++ src/build.sh
@@ -3,7 +3,6 @@ g++ -o sitega_thr_dist_mat.exe sitega_thr_dist_mat.cpp
 g++ -o andy0bsn5.exe andy0bsn5.cpp
 g++ -o andy1_mat.exe andy1_mat.cpp
 g++ -o andy05.exe andy05.cpp
-g++ -o background_genome_mono.exe background_genome_mono.cpp
 chmod a+x andy0bsn5.exe
 chmod a+x andy1_mat.exe
 chmod a+x andy05.exe
