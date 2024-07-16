3695f95d6eb4235d513b0c4ed791b25c14cc867d
Victor Levitsky <68056117+parthian-sterlet@users.noreply.github.com>
2024-07-16 18:43:04 +0700

    Update build.sh

--- src/build.sh
+++ src/build.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 g++ -o sitega_thr_dist_mat.exe sitega_thr_dist_mat.cpp
-g++ -o andy0bsn5.exe andy0bsn5.cpp
+g++ -o andy0bsn5cell.exe andy0bsn5cell.cpp
 g++ -o andy1_mat.exe andy1_mat.cpp
-g++ -o andy05.exe andy05.cpp
-chmod a+x andy0bsn5.exe
+g++ -o andy05cell.exe andy05cell.cpp
+chmod a+x andy0bsn5cell.exe
 chmod a+x andy1_mat.exe
-chmod a+x andy05.exe
+chmod a+x andy05cell.exe
 chmod a+x sitega_thr_dist_mat.exe
 chmod a+x bootstrap
 chmod a+x scan 
